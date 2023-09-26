9089602ba31ee4e0871c25d1bada341988adfa42
DevinParentice <devinparentice@gmail.com>
2023-09-25 20:30:37 -0400

    Fixed bug when adding new element to watch

--- watcher.py
+++ watcher.py
@@ -4,7 +4,6 @@ import json
 import threading
 import os
 
-from datetime import datetime
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
@@ -23,7 +22,7 @@ RECIPIENT_EMAIL_ADDRESS = os.environ.get("RECIPIENT_EMAIL")
 
 
 def send_email_alert(website, stale_content, new_content, log):
-    """Sends an email alert. The subject and body will be the same."""
+    """Sends an email alert."""
     website_name = website["name"]
     log.info(f"{f'[{website_name}]':<15} | Sending an email alert")
     changed_classes = []
@@ -75,13 +74,12 @@ def fetch_html(string, website):
     return new_content
 
 
-def webpage_was_changed(website, log):
-    """Returns true if the webpage was changed, otherwise false."""
+def webpage_has_changed(website, log):
+    """Checks if the webpage has changed."""
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
     }
 
-    # create the previous_content.txt if it doesn't exist
     if not os.path.exists("content.json"):
         open("content.json", "w+").close()
     website_name = website["name"]
@@ -90,27 +88,30 @@ def webpage_was_changed(website, log):
         response = requests.get(website["url"], headers=headers)
 
         with open("content.json") as f:
-            previous_content = json.load(f)
+            saved_content = json.load(f)
 
         new_content = fetch_html(response.text, website)
 
-        if website_name not in previous_content:
-            previous_content[website_name] = {}
-        if new_content == previous_content[website_name]:
+        if website_name not in saved_content:
+            saved_content[website_name] = {}
+        if new_content == saved_content[website_name]:
             log.info(f"{f'[{website_name}]':<15} | Check finished, no changes")
         else:
-            stale_content = previous_content[website_name]
-            previous_content[website_name] = new_content
+            stale_content = saved_content[website_name]
+            saved_content[website_name] = new_content
             with open("content.json", "w", encoding="utf-8") as f:
-                json.dump(previous_content, f, ensure_ascii=False)
+                json.dump(saved_content, f, ensure_ascii=False)
             log.info(f"{f'[{website_name}]':<15} | Check finished, changes detected")
-            if stale_content != {}:
-                send_email_alert(website, stale_content, previous_content[website_name], log)
+            if stale_content == {}:
+                log.info(f"{f'[{website_name}]':<15} | First time website was scanned, skipping email")
+            elif stale_content.keys() != new_content.keys():
+                log.info(f"{f'[{website_name}]':<15} | First time item was scanned, skipping email")
+            else:
+                send_email_alert(website, stale_content, new_content, log)
         time.sleep(website["delay"] * 60)
 
 
 def main():
-    now = datetime.now()
     log = logging.getLogger(__name__)
     logging.basicConfig(
         level=os.environ.get("LOGLEVEL", "INFO"),
@@ -126,7 +127,7 @@ def main():
         json_data = json.load(config)
 
     for website in json_data["websites"]:
-        threading.Thread(target=webpage_was_changed, args=(website, log)).start()
+        threading.Thread(target=webpage_has_changed, args=(website, log)).start()
 
 
 if __name__ == "__main__":
